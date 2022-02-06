153175ca4b0070e6e474da51d3eb8b4b12065901
Kemari <kemaric@gmail.com>
2022-02-05 22:51:46 -0500

    updates about page

--- loaded-question-pwa/src/views/About.vue
+++ loaded-question-pwa/src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Loaded Questions</h1>
+    <p>
+      This is the game where you can play with a group of 4-6 friends (or strangers) and get to know them REALLY well.
+      User ask a question to other players, get responses back and the player matches the responses with their friend. They then move up a space on the board for each match they get right.
+    </p>
   </div>
 </template>
