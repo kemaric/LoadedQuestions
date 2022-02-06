a7517697ed6bac20e711f1b66e85ced3c2f82a15
Kemari <kemaric@gmail.com>
2022-02-05 22:53:19 -0500

    updates Home to toggle create game

--- loaded-question-pwa/src/views/Home.vue
+++ loaded-question-pwa/src/views/Home.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <CreateGame :minPlayers="4"></CreateGame>
+    <div class="logo">
+      <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+    <Login></Login>
+    <CreateGame v-if="player" :minPlayers="4"></CreateGame>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
+import Login from '@/components/Login.vue';
 import CreateGame from '@/components/CreateGame.vue'
-
+import { mapGetters } from 'vuex';
 export default {
   name: 'Home',
   components: {
-    // HelloWorld,
+    Login,
     CreateGame
+  },
+  computed: {
+    ...mapGetters({ player: 'getCurrentUser' })
   }
 }
 </script>
